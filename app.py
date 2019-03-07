72b51b22bb73b94fd8a102f25acbf5337617543e
florimondmanca <florimond.manca@gmail.com>
2019-03-07 07:27:41 +0100

    fix: connection to socket.io server

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import socketio
 app = App()
 templates = Templates(app)
 
-sio = socketio.AsyncServer()
-app.mount("/sio", sio)
+sio = socketio.AsyncServer(async_mode="asgi")
+app.mount("/sio", socketio.ASGIApp(sio))
 app.mount("/socket.io", static("node_modules/socket.io-client/dist"))
 
 
@@ -14,5 +14,19 @@ async def index(req, res):
     res.html = await templates.render("index.html")
 
 
+class EchoChatNamespace(socketio.AsyncNamespace):
+    def on_connect(self, sid, environ):
+        sio.enter_room(sid, "users")
+
+    def on_disconnect(self, sid):
+        sio.leave_room(sid, "users")
+
+    async def on_message(self, sid, data):
+        await self.emit("response", data, room="users")
+
+
+sio.register_namespace(EchoChatNamespace("/chat"))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
